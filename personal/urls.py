acaa0a231b4bb1c4b6d4703760a068810d240b24
Sheldon Rong <sheldon@freelancer.comm>
2014-01-03 17:17:03 +1100

    add chat server example, still need integration

--- personal/urls.py
+++ personal/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.auth.views import login, logout
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('MySite.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', login, {'template_name': 'login.html'}),
+    url(r'^logout/$', logout, {'next_page': '/'})
 )
